a44e02f72c0520cc1e48e7bf62b504235febf0d9
yanivin08 <36818136+yanivin08@users.noreply.github.com>
2020-09-25 19:09:54 +0800

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const { modelNames } = require("mongoose");
+
+module.exports = {
+    mongoURI: "mongodb://yanivin08:masayahin08@cluster0-shard-00-00-lbefq.mongodb.net:27017,cluster0-shard-00-01-lbefq.mongodb.net:27017,cluster0-shard-00-02-lbefq.mongodb.net:27017/kumusta?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
