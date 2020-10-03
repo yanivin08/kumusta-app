a271cc58c1ce09cac0080770f6fc9b7fad7ef2e6
asumilhig <asumi1490@yahoo.com>
2020-10-04 07:45:44 +0800

    Fixed error handler Reg form

--- routes/User.js
+++ routes/User.js
@@ -19,71 +19,64 @@ router.post('/register', (req,res)=>{
     let errors = [];
 
     if(!name || !email || !password || !password2){
-        errors.push({ msg: 'Please fill in all fields' });
+        res.send({
+            success: false,
+            msg: 'Please fill in all fields' 
+        });
     }
 
     if(password !== password2){
-        errors.push({ msg: 'Passwords do not match' });
+        res.send({
+            success: false,
+            msg: 'Passwords did not match' 
+        });
     }
 
     if(password.length < 6){
-        errors.push({ msg: 'Password should be at least 6 characters'})
-    }
-
-    if(errors.length > 0){
-        // res.send('register', {
-        //     errors,
-        //     name,
-        //     email,
-        //     password,
-        //     password2
-        // });
         res.send({
             success: false,
-            msg: "Error occurs, please try again.",
-            err_data: errors
-        })
-
-    }else{
-        //validation passed
-        User.findOne({ email: email })
-            .then(user => {
-                if(user){
-                    //user exist
-                    errors.push({ msg: 'Email is already registered' })
-                    res.send({
-                        success: false,
-                        msg: 'Email is already registered',
-                        err_data: errors
-                    });
-                } else {
-                    const newUser = new User({
-                        name,
-                        email,
-                        password
-                    });
-
-                    //hash password
-                    bcrypt.genSalt(10, (err, salt) => 
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if(err) throw err;
-                            //set password to hashed
-                            newUser.password = hash;
-                            //save user
-                            newUser.save()
-                                .then(user => {
-                                    res.send({
-                                        success: true,
-                                        msg: 'You are now successfully registered!'
-                                    })
-                                    res.redirect('./login')
-                                })
-                                .catch(err => console.log(err));
-                        })
-                    );
-                }
-            });
+            msg: 'Password should be at least 6 characters' 
+        });
     }
+    
+    //validation passed
+    User.findOne({ email: email })
+        .then(user => {
+            if(user){
+                //user exist
+                errors.push({ msg: 'Email is already registered' })
+                res.send({
+                    success: false,
+                    msg: 'Email is already registered',
+                    err_data: errors
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                //hash password
+                bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        //set password to hashed
+                        newUser.password = hash;
+                        //save user
+                        newUser.save()
+                            .then(user => {
+                                res.send({
+                                    success: true,
+                                    msg: 'You are now successfully registered!'
+                                })
+                                res.redirect('./login')
+                            })
+                            .catch(err => console.log(err));
+                    })
+                );
+            }
+        });
 
 })
 
