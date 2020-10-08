8fe52b033b553cd282526a522629ca589325a89a
asumilhig <asumi1490@yahoo.com>
2020-10-09 02:57:24 +0800

    changed path

--- server.js
+++ server.js
@@ -80,19 +80,18 @@ app.get('/isproduction', (req, res) => {
 app.use('/user', require('./routes/User'));
 app.use('/chat', require('./routes/Chat'));
 
-// // Serve static assests if in production
-// if ( process.env.NODE_ENV === 'production' ) {
-//   // set static folder
-//   app.use( express.static('client/build') );
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use( express.static(path.join(__dirname, 'client/build')));
 
-//   app.get('*', (req,res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'client/build/index.html'));
+  });
+}
 
-app.use(express.static('client/build'));
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
 });
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
