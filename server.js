514e0bdd28afd818c8dd7cf990c3b3094ef67aaf
asumilhig <asumi1490@yahoo.com>
2020-10-09 02:27:42 +0800

    built react-app

--- server.js
+++ server.js
@@ -80,14 +80,19 @@ app.get('/isproduction', (req, res) => {
 app.use('/user', require('./routes/User'));
 app.use('/chat', require('./routes/Chat'));
 
-// Serve static assests if in production
-if ( process.env.NODE_ENV === 'production' ) {
-  // set static folder
-  app.use( express.static('client/build') );
+// // Serve static assests if in production
+// if ( process.env.NODE_ENV === 'production' ) {
+//   // set static folder
+//   app.use( express.static('client/build') );
 
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req,res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.use(express.static('client/build'));
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
