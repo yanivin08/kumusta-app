8c8e3f95159f92178a6a728088ea2474e7e5a194
asumilhig <asumi1490@yahoo.com>
2020-10-08 19:11:29 +0800

    add env

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use('/chat', require('./routes/Chat'));
 // Serve static assests if in production
 if ( process.env.NODE_ENV === 'production' ) {
   // set static folder
-  app.use( express.static('client/build'));
+  app.use( express.static('client/build') );
 
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
