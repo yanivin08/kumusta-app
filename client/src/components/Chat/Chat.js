c87a128cf16df7d0c51cc5f87bc1e5270d70eed6
03yendoR <rodney12302k8@gmail.com>
2020-10-09 17:58:17 +0800

    make it better

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 import './Chat.css';
 
 //Chaneg ENDPOINT to URL of Heroku
-const ENDPOINT = 'localhost:5000';
+const ENDPOINT = 'https://kumusta-na-aking-mahal.herokuapp.com/';
 
 let socket;
 
